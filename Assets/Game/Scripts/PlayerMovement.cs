282cf5949a409f84d4c36471fd3c9e5dd1559721
AxetheRoad <AxetheRoad@hotmail.com>
2022-09-15 09:51:45 -0700

    test

--- Assets/Game/Scripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rbPlayer;
+    private Vector3 direction = Vector3.zero;
+    public float speed = 10.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rbPlayer = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        rbPlayer.AddForce(direction * speed, ForceMode.Force);
+
+        if (transform.position.z > 100)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 100);
+        }
+        else if (transform.position.z < -100)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -100);
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horMove = Input.GetAxis("Horizontal");
+        float verMove = Input.GetAxis("Vertical");
+        direction = new Vector3(verMove, 0, horMove);
     }
 }
